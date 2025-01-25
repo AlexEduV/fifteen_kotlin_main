d0d7d0810db51b294dfb81daccfd35c8633333b7
Alexander Aks'onov <m.mirvua@gmail.com>
2025-01-25 21:31:33 +0200

    wanted to add material effect to tile, but no results;

--- fifteen_kotlin/app/src/main/java/com/alexaksonov/fifteen_kotlin/ui/widgets/Tile.kt
+++ fifteen_kotlin/app/src/main/java/com/alexaksonov/fifteen_kotlin/ui/widgets/Tile.kt
@@ -4,9 +4,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,25 +32,29 @@ fun Tile(
     onClick: () -> Boolean,
 ) {
 
-    Box(
-        contentAlignment = Alignment.Center,
+    Surface(
         modifier = modifier
             .padding(4.dp)
             .clip(RoundedCornerShape(12.dp))
             .border(1.dp, AppColors.Silver, RoundedCornerShape(12.dp))
-            .background(backgroundColor)
-            .size(80.dp)
-            .clickable {
-                onClick()
-            }
+            .size(80.dp),
+        shadowElevation = 16.dp,
+        color = backgroundColor,
+        onClick = { onClick() }
     ) {
-        Text(tileLabel,
-            style = TextStyle(
-                fontSize = 32.sp,
-                color = AppColors.Silver,
-                fontWeight = FontWeight.W600,
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                tileLabel,
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    color = AppColors.Silver,
+                    fontWeight = FontWeight.W600,
+                )
             )
-        )
+        }
     }
 }
 
