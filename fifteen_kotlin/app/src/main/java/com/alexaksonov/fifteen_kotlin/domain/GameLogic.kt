8cd65a0829ac5ead35e1a58821899016ece356d9
Alexander Aks'onov <m.mirvua@gmail.com>
2025-01-25 12:45:54 +0200

    started working on the game logic;

---
+++ fifteen_kotlin/app/src/main/java/com/alexaksonov/fifteen_kotlin/domain/GameLogic.kt
@@ -0,0 +1,12 @@
+package com.alexaksonov.fifteen_kotlin.domain
+
+class GameLogic {
+
+    var gameTiles = mutableListOf(1..16)
+
+    //generate a new sequence
+    fun generateSequence(): MutableList<IntRange> {
+        return 
+    }
+
+}
