a7307ce1a54a4c8a253a104007693263649b15ef
Alexander Aks'onov <m.mirvua@gmail.com>
2025-01-25 13:32:03 +0200

    working on UI;

--- fifteen_kotlin/app/src/main/java/com/alexaksonov/fifteen_kotlin/MainActivity.kt
+++ fifteen_kotlin/app/src/main/java/com/alexaksonov/fifteen_kotlin/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.alexaksonov.fifteen_kotlin.ui.theme.FifteenKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +42,40 @@ fun MainScreen(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun TileGrid(modifier: Modifier = Modifier) {
+
+    Column {
+        repeat(4) {
+            Row {
+                repeat(4) {
+                    Tile("12")
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GridPreview() {
+    FifteenKotlinTheme {
+        TileGrid()
+    }
+}
+
+
+@Composable
 fun Tile(tileLabel: String, modifier: Modifier = Modifier) {
-    Text(tileLabel)
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .clip(RoundedCornerShape(16.dp))
+            .width(50.dp)
+            .height(50.dp),
+    ) {
+        Text(tileLabel)
+    }
 }
 
 @Preview(showBackground = true)
